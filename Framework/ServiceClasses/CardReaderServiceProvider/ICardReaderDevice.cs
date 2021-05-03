b06b48591980a744c98bf3c91306e6780dedf7f2
KaderBaadoud-KAL <61241601+KaderBaadoud-KAL@users.noreply.github.com>
2021-05-03 14:58:32 +0100

    Update headers as this file is not manually changed

--- Framework/ServiceClasses/CardReaderServiceProvider/ICardReaderDevice.cs
+++ Framework/ServiceClasses/CardReaderServiceProvider/ICardReaderDevice.cs
@@ -3,7 +3,6 @@
  * KAL ATM Software GmbH licenses this file to you under the MIT license.
  * See the LICENSE file in the project root for more information.
  *
- * This file was created automatically as part of the XFS4IoT CardReader interface.
  * ICardReaderDevice.cs uses automatically generated parts. 
 \***********************************************************************************************/
 
