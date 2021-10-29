cc59ec58166d0d11711923827a52ae7fb615a406
Seishi Nakano <Seishi@kal.com>
2021-10-29 10:21:37 +0200

    SetLight and SetVersion commands are not supported for Nov workshop and planning to support it in Dec workshop

--- Framework/ServiceClasses/LightsServiceProvider/Handlers/SetLightHandler.cs
+++ Framework/ServiceClasses/LightsServiceProvider/Handlers/SetLightHandler.cs
@@ -3,10 +3,7 @@
  * KAL ATM Software GmbH licenses this file to you under the MIT license.
  * See the LICENSE file in the project root for more information.
  *
- * This file was created automatically as part of the XFS4IoT Lights interface.
- * SetLightHandler.cs uses automatically generated parts.
 \***********************************************************************************************/
-
 
 using System;
 using System.Threading.Tasks;
@@ -15,6 +12,8 @@ using XFS4IoT;
 using XFS4IoTServer;
 using XFS4IoT.Lights.Commands;
 using XFS4IoT.Lights.Completions;
+using XFS4IoT.Completions;
+
 
 namespace XFS4IoTFramework.Lights
 {
@@ -23,14 +22,8 @@ namespace XFS4IoTFramework.Lights
 
         private Task<SetLightCompletion.PayloadData> HandleSetLight(ISetLightEvents events, SetLightCommand setLight, CancellationToken cancel)
         {
-            //ToDo: Implement HandleSetLight for Lights.
-            
-            #if DEBUG
-                throw new NotImplementedException("HandleSetLight for Lights is not implemented in SetLightHandler.cs");
-            #else
-                #error HandleSetLight for Lights is not implemented in SetLightHandler.cs
-            #endif
+            return Task.FromResult(new SetLightCompletion.PayloadData(MessagePayload.CompletionCodeEnum.UnsupportedCommand,
+                                                                      $"SetLight commnad is not supported yet."));
         }
-
     }
 }
